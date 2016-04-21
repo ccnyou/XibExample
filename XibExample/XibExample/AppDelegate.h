c63ab484a97cc8911a61fe04d72fbac8a2769aa7
ccnyou <ccnyou@qq.com>
2016-04-21 22:09:33 +0800

    + create project

---
+++ XibExample/XibExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XibExample
+//
+//  Created by 聪宁陈 on 16/4/21.
+//  Copyright © 2016年 ccnyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XibExample/XibExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XibExample
+//
+//  Created by 聪宁陈 on 16/4/21.
+//  Copyright © 2016年 ccnyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
