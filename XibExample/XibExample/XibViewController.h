caa59d1007834353f287ae6586b7ebc941f29177
ccnyou <ccnyou@qq.com>
2016-04-21 22:12:30 +0800

    + add a empty view controller class

---
+++ XibExample/XibExample/XibViewController.h
@@ -0,0 +1,13 @@
+//
+//  XibViewController.h
+//  XibExample
+//
+//  Created by 聪宁陈 on 16/4/21.
+//  Copyright © 2016年 ccnyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XibViewController : UIViewController
+
+@end
